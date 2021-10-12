f10e23553fc10ea945411e1ae9deee469b96b816
Hafni-anusha <hafnianushame@gmail.com>
2021-10-12 05:27:23 +0000

    first commit

---
+++ palcare1/palcare1/doctype/enrolment_section/enrolment_section.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Palcare and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Enrolment Section', {
+	// refresh: function(frm) {
+
+	// }
+});
