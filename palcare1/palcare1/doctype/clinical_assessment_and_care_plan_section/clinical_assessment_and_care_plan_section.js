ede53ad37078f251cc82d3ff4d19e4c1888e8c07
Hafni-anusha <hafnianushame@gmail.com>
2021-10-20 07:14:09 +0000

    second-commit

---
+++ palcare1/palcare1/doctype/clinical_assessment_and_care_plan_section/clinical_assessment_and_care_plan_section.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Palcare and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Clinical Assessment And Care Plan Section', {
+	// refresh: function(frm) {
+
+	// }
+});
